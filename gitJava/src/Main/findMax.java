c399890887b3278d9e376c878dc54805b3e26eef
Enjoy <THINK@DESKTOP-UGDRGL6>
2020-10-13 23:43:57 +0800

    Finish by 姬亮

---
+++ gitJava/src/Main/findMax.java
@@ -0,0 +1,52 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class findMax {//最大值，平均分，及格率。姬亮
+
+	public Integer FindMax(ArrayList<Integer> arr) {
+		/*
+		 * Integer Max = arr.get(0); if(Max<arr.get(arr.size()-1)) { Max =
+		 * arr.lastIndexOf(arr); }
+		 */
+		int max = Collections.max(arr); 
+		return max;
+	}
+
+
+
+
+	public float FindAverage (ArrayList<Integer> arr) {
+		int sum = 0;
+		float average = 0;
+		for (Integer integer : arr) {
+			sum = sum +integer;
+		}
+		average = (sum/arr.size());
+		return average;
+	
+
+	}
+	public float FindPassRate (ArrayList<Integer> arr) {
+		int count = 0;
+		float PassRate=0;
+//		for(int i=0;i<arr.length;i++) {
+//			if (arr[i]>60) {
+//				count ++;
+//			}else {
+//			continue;
+//		}
+		for (int obj : arr) {
+			if (obj>=60) {
+				count ++;
+			}
+		
+		}
+		PassRate =	(count/arr.size());
+		return PassRate;
+		}
+}
+
+	
